7ca359b1620bdd27927367ec6eaf890b4f7b2018
Lmakinaa <jaija.ismail@gmail.com>
2025-05-16 16:49:02 +0100

    added deploy script

---
+++ start.sh
@@ -0,0 +1,8 @@
+docker run -d \
+  --name portfolio-db \
+  -e POSTGRES_USER=testusername \
+  -e POSTGRES_PASSWORD=testpass \
+  -e POSTGRES_DB=portfolio_db \
+  -p 5432:5432 \
+  postgres && cd srcs && export DATABASE_URL=postgresql://testusername:testpass@localhost:5432/portfolio_db &&\
+  npx prisma generate && npx prisma migrate deploy && npm run start
